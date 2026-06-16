d64f95f635be085780277572ef80044aef988b03
Moder112 <olek@mogila.net>
2026-06-16 18:43:41 +0200

    feat: add splitting into entries, length limits

--- processing.py
+++ processing.py
@@ -29,46 +29,94 @@ def comment_to_labels(l: str):
     
     return l.split(",")
 
+RARE_LABELS = [
+    "164931005",
+    "284470004",
+    "426783006",
+    "429622005"   
+]
 
-def process_entry(data_dir, record_name):
+def contains_rare_labels(labels: list):
+    for l in RARE_LABELS:
+        if labels.count(l) > 0:
+            return True    
+    return False
+
+def generate_partitions(length, fs, length_cap):
+    max_seg = length / length_cap
+    end = math.floor(max_seg) * length_cap * fs
+    r = range(0, end + 1, length_cap * fs)
+    
+    partitions = []
+
+    start = 0    
+    for i in r[1:]:
+        partitions.append((start, i))
+        start = i
+        
+    return partitions
+    
+
+def process_entry(data_dir, record_name, split_extra_samples, length_cap):
     record = wfdb.rdrecord(os.path.join(data_dir, record_name))
     fs = record.fs
-    data = []
     
+    len_s = record.sig_len / fs
     
-    for lead in range(len(record.sig_name)):
-        raw_signal = record.p_signal[:, lead]
-        b, a = signal.butter(3, [0.5, 50], btype='bandpass', fs=fs)
-        filtered_signal = signal.filtfilt(b, a, raw_signal)
+    if len_s < length_cap:
+        print(f"Skipping record {record_name} due to short length ({len_s})")
+        return [] 
+    
+    partitions = generate_partitions(len_s, fs, length_cap)
+    
+    labels = comment_to_labels(record.comments[2])
+    has_rare_labels = contains_rare_labels(labels)
+    
+    if not has_rare_labels or not split_extra_samples:
+        partitions = [partitions[0]]
+    
+    new_entries = []
+    
+    for start, end in partitions:
+        data = []
+        for lead in range(len(record.sig_name)):
+            raw_signal = record.p_signal[start:end, lead]
+            b, a = signal.butter(3, [0.5, 50], btype='bandpass', fs=fs)
+            filtered_signal = signal.filtfilt(b, a, raw_signal)
 
-        _a, _b, spect = signal.spectrogram(filtered_signal, fs=fs, nperseg=32)
+            _a, _b, spect = signal.spectrogram(filtered_signal, fs=fs, nperseg=32)
+            
+            spect_db = 10 * np.log10(spect + 1e-10)
+            data.append(spect_db)
+            
         
-        spect_db = 10 * np.log10(spect + 1e-10)
-        data.append(spect_db)
+        new_entries.append({
+            "record": record,
+            "data": np.array(data),
+            "label": labels 
+        })
         
-        
-    return {
-        "record": record,
-        "data": np.array(data),
-        "label": comment_to_labels(record.comments[2])
-    }
+    return new_entries
     
-def precompute_data(data_dir, patients):
+def precompute_data(data_dir, patients, split_extra_samples, length_cap):
     entries = []
     i = 1
     
     for patient in patients:
-        entries.append(process_entry(data_dir, patient['id']))
-        print(f"Precomputing patient no. {i} / {len(patients)}")
+        new_entries = process_entry(data_dir, patient['id'], split_extra_samples, length_cap)
+        for entry in new_entries:
+            entries.append(entry)
+            
+        print(f"Precomputing patient no. {i} / {len(patients)}, {len(new_entries)} entries added")
         i += 1
         
     return entries
 
-def precompute_and_save(input_folder, output):
+def precompute_and_save(input_folder, output, split_extra_samples, length_cap):
     patients = load_all_metadata(input_folder)
 
     print("Computing data")
-    precomp = precompute_data(input_folder, patients)
+    precomp = precompute_data(input_folder, patients, split_extra_samples, length_cap)
     
     print("Writing pickle")
     with open(output, "wb") as f:
